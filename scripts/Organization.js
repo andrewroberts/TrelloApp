df47cec189685088669b4c0c804f66c91891d99f
Andrew Roberts <andrew@roberts.net>
2015-11-22 22:25:14 +0000

    updated by GasGit automation

---
+++ scripts/Organization.js
@@ -0,0 +1,24 @@
+/**
+ * @classdesc
+ * This class defines an Trello Organization.
+ * @constructor
+*/
+
+function Organizations() {
+
+ /**
+  * @return {string} The id of this Segment
+  */
+  Organizations.prototype.getDisplayName = function() {
+    return this.wrapped.displayName;
+  }
+  
+  Organizations.prototype.getId = function() {
+    return this.wrapped.id;
+  }
+
+  Organizations.prototype.GetbillableMemberCount = function() {
+    return this.wrapped.billableMemberCount;
+  }
+  
+} // Organizations
